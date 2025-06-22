c1dfa64d5bc7d4505939b1e632750920bc047ac9
Strilets-Glib-FI41mn <181478164+Strilets-Glib-FI41mn@users.noreply.github.com>
2025-06-22 22:23:29 +0300

    Added hazelcast_rest and used it for logging

--- logging-service/src/main.rs
+++ logging-service/src/main.rs
@@ -3,13 +3,15 @@ use std::{collections::HashMap, env, io::{prelude::*, BufReader}, net::{TcpListe
 use http_reader::HttpReader;
 
 use uuid::Uuid;
-
+use hazelcast_rest::HazelcastRestClient;
 
 fn main() {
     //let mut port = "7878".to_owned();
     let storage = Arc::new(Mutex::new(HashMap::new()));
     let mut logging_service_port = "9898".to_owned();
+    let mut hazelcast_port = "5701".to_owned();
     let mut show_debug = false;
+    let mut hazelcast_map = "map".to_owned();
     let args: Vec<_> = env::args().skip(1).collect();
     for arg in &args{
         let (key, value) = 
@@ -31,6 +33,19 @@ fn main() {
                         }
                     }
                 }
+            }"--hazelcast_port" => {
+                if let Some(port_string) = &value{
+                    if let Ok(val) = port_string.parse::<i32>(){
+                        if val < 65535{
+                            hazelcast_port = port_string.clone();
+                        }
+                    }
+                }
+            }
+            "--hazelcast_map" => {
+                if let Some(val) = &value{
+                    hazelcast_map = val.into();
+                }
             }
             "-d" | "--debug" =>{
                 show_debug = true;
@@ -46,18 +61,20 @@ fn main() {
         }
         println!("using port {logging_service_port}");
     }
-    let logging_adress: String = "127.0.0.1:".to_owned() + &logging_service_port;
+    let ip_address = "127.0.0.1:";
+    let logging_adress: String = ip_address.to_owned() + &logging_service_port;
+    //let hazelcast_adress: String = ip_address.to_owned() + &hazelcast_port;
     let listener = TcpListener::bind(logging_adress).unwrap();
-
+    //let hazelcast_port = Arc::<String>::new(hazelcast_port);
     
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream, storage.clone(), show_debug);
+        handle_connection(stream, storage.clone(), show_debug, ip_address.to_owned(), hazelcast_port.clone(), &hazelcast_map);
         //handle_connection(stream)
     }
 }
 
-fn handle_connection(mut stream: TcpStream, data: Arc<Mutex<HashMap<Uuid, String>>>, show_debug: bool){
+fn handle_connection(mut stream: TcpStream, data: Arc<Mutex<HashMap<Uuid, String>>>, show_debug: bool, ip_address: String, hazelcast_port: String, hazelcast_map: &str){
     let mut buf_reader = BufReader::new(&stream);
     let mut line_consumer = HttpReader::new(&mut buf_reader);
     let request = line_consumer.make_request();
@@ -74,7 +91,11 @@ fn handle_connection(mut stream: TcpStream, data: Arc<Mutex<HashMap<Uuid, String
                         
                         println!("Got POST request with data: '{:#?}'", &values);
                         let mut storage = data.lock().unwrap();
-                        storage.insert(val, values[1].to_owned());
+                        storage.insert(val.clone(), values[1].to_owned().clone());
+                        
+                        let client = HazelcastRestClient::new(&ip_address, &hazelcast_port);
+                        let res = client.map_put::<String>(hazelcast_map,&Into::<String>::into(val), values[1].to_owned());
+                        println!("{res:?}");
 
                     let response = "HTTP/1.1 201 Created\r\n\r\n";
                     stream.write_all(response.as_bytes()).unwrap();
